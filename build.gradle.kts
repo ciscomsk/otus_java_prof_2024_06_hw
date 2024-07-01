c3fa078192517dc3802bee31c7fbfa35e6725100
ciscomsk <kuznetzoff@gmail.com>
2024-07-02 00:36:27 +0300

    hw_1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,16 @@ subprojects {
 
     pluginManager.apply("java")
 
+    extensions.configure<JavaPluginExtension> {
+        toolchain {
+            languageVersion = JavaLanguageVersion.of(21)
+        }
+    }
+
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.addAll(listOf("-Xlint:all"))
+    }
+
     group = "ru.otus"
     version = "1.0-SNAPSHOT"
 }
